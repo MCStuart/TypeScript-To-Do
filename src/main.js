51c5f27b156ab0cedc0de0ba6e96cc68d519f706
lindsey baker and stuart mckay <stuart@>
2019-06-10 10:12:41 -0700

    Add consolelog

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-//require('exports-loader?file!./bootstrap/js/dist/.js')
-// import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './css/styles.css';
-//$(document).ready(function() {
+import { Task } from './../app/to-do.js';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
 
-//});
+$(document).ready(function() {
+  for(var task of tasks){
+    console.log(task);
+  }
+});
